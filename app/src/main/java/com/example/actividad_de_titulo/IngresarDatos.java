720562eb5a8fc98720bbe9950bfd85cbb1038024
DiegoSV <diego.sandovalvallejos21@gmail.com>
2024-11-24 01:42:09 -0300

    ver datos

--- app/src/main/java/com/example/actividad_de_titulo/IngresarDatos.java
+++ app/src/main/java/com/example/actividad_de_titulo/IngresarDatos.java
@@ -15,7 +15,7 @@ public class IngresarDatos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ingresar_datos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.inputLayoutSearch), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
